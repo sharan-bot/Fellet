178d998d2255890303b564ea1b50af93d6f9015b
Sharan K <72678430+sharan-bot@users.noreply.github.com>
2024-05-06 16:01:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name                =   "Fellet",
+    version             =   '1.2',
+    description         =   "The Multi-Tool Web Vulnerability Scanner.",
+    long_description    =   README,
+    long_description_content_type = "text/markdown",
+    url                 =   "https://github.com/sharan-bot/Fellet",
+    author              =   "Sharan",
+    py_modules          =   ['rapidscan',],
+    install_requires    =   [],
+    python_requires=">=3.6",
+)
